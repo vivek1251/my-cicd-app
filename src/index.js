990ccfc3b631cb41b92e94598295108ab02db846
vivek <vivekbommalla1251@gmail.com>
2026-03-12 07:14:24 +0530

    Update to Version 2.0

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! Version 1.0');
+  res.send('Hello from CI/CD Pipeline! Version 2.0');
 });
 
 app.listen(PORT, () => {
